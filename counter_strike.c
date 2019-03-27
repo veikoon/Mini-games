2beecd697aa59b61f28cbff5cbbc28b0d17348ed
Vincent Monnot <vincent.monnot@edu.esiee.fr>
2019-03-27 17:40:59 +0100

    counter strike working

--- counter_strike.c
+++ counter_strike.c
@@ -18,78 +18,86 @@ struct Player
 	int vie;
 };
 
-Player joueur;
-Player ordi;
-Arme ak_47;
-Arme m4a1_s;
+void action(Player *ordi, Player *joueur);
+void iaction(Player *ordi, Player *joueur);
+void act(Player *one, Player *two, int act);
+void tirer(Player *zbeub);
+void baisser(Player *zbeub);
+int welcome(Player *joueur);
+int end(Player *one, Player *two);
 
-void action(void);
-void iaction(void);
-void tirer(Player zbeub);
-void baisser(Player zbeub);
-void welcome(void);
+int end(Player *one, Player *two)
+{
+	if ((*one).vie <= 0){
+		printf("%s à perdu !\n", (*one).name);
+		return (0);
+	}
 
-void baisser(Player zbeub){
-	zbeub.arme.min += 1;
-	zbeub.arme.max += 1;
-	printf("%s s'est baissé\n", zbeub.name);
+	if ((*two).vie <= 0){
+		printf("%s à perdu !\n", (*two).name);
+		return (0);
+	}
+
+	return (1);
 }
 
-void action(void)
+void baisser(Player *zbeub){
+	(*zbeub).arme.min += 1;
+	(*zbeub).arme.max += 1;
+	printf("%s s'est baissé\n", (*zbeub).name);
+}
+
+void act(Player *one, Player *two, int act)
 {
-	int act;
-	printf("choisissez entre (1) vous baisser  (2) tirer\n>");
-	scanf("%d", &act);
-	if (act == 2){
-		tirer(ordi);
-	}
 	if (act == 1){
-		baisser(joueur);
+		tirer(two);
 	}
+
+	if (act == 0){
+		baisser(one);
+	}
+
+	printf("%d %d\n", (*one).vie, (*two).vie);
 }
 
-void iaction(void)
+void action(Player *ordi, Player *joueur)
 {
-	srand(time(NULL));
+	int actions;
+	printf("choisissez entre (0) vous baisser  (1) tirer\n>");
+	scanf("%d", &actions);
+	act(joueur, ordi, actions);
+}
+
+void iaction(Player *ordi, Player *joueur)
+{
 	int random = (rand() % 2);
-	if (random == 1){
-		tirer(joueur);
-	}
-	if (random == 0){
-		baisser(ordi);
-	}
+	act(ordi, joueur, random);
 }
 
-void tirer(Player zbeub)
+void tirer(Player *zbeub)
 {
-	srand(time(NULL));
 	int nombreMystere = (rand() % (5)) + 1;
+
 	switch (nombreMystere){
 	case 1:
 	case 2:
-		printf("%s à été touché !\n", zbeub.name);
-		srand(time(NULL));
-		int random = (rand() % (zbeub.arme.max -
-		zbeub.arme.min + 1)) + zbeub.arme.min;
-		zbeub.vie -= random;
+		printf("%s à été touché !\n", (*zbeub).name);
+		int random = (rand() % ((*zbeub).arme.max -
+		(*zbeub).arme.min + 1)) + (*zbeub).arme.min;
+		(*zbeub).vie -= random;
 		break;
 	case 3:
 	case 4:
-		printf("%s à été raté !\n", zbeub.name);
+		printf("%s à été raté !\n", (*zbeub).name);
 		break;
 	case 5:
 		printf("Headshot !\n");
-		zbeub.vie = 0;
+		(*zbeub).vie = 0;
 	}
-	if (joueur.vie <= 0){
-		printf("%s à perdu !\n", joueur.name);
-	}
-	if (ordi.vie <= 0){
-		printf("%s à perdu !\n", ordi.name);
-	}
+
 }
 
-void welcome(void)
+int welcome(Player *joueur)
 {
 	int choix;
 	char nom[50];
@@ -97,37 +105,61 @@ void welcome(void)
 	scanf("%s", nom);
 	printf("Bienvenu dans Counter Strike\n");
 	printf("Première choisissez une arme entre :");
-	printf("(1) AK-47  (2) M4A1-S\n>");
+	printf("(0) AK-47  (1) M4A1-S\n>");
 	scanf("%d", &choix);
-	if (choix == 1){
-		joueur.arme = ak_47;
-	}
-	if (choix == 2){
-		joueur.arme = m4a1_s;
-	}
-	strcpy(joueur.name, nom);
+	strcpy((*joueur).name, nom);
+	return (choix);
 }
 
 int main(int argc, char const *argv[])
 {
-	ak_47.max = 4;
-	ak_47.min = 2;
-	m4a1_s.max = 5;
-	m4a1_s.min = 1;
-	ordi.arme = ak_47;
-	joueur.vie = 20;
-	ordi.vie = 20;
+	int stop;
+	Player joueur;
+	Player ordi;
+	Arme ak_47;
+	Arme m4a1_s;
+	stop		= 1;
+	ak_47.max 	= 4;
+	ak_47.min 	= 2;
+	m4a1_s.max 	= 5;
+	m4a1_s.min 	= 1;
+	ordi.arme 	= ak_47;
 	strcpy(ordi.name, "L'ordi");
+	srand(time(NULL));
+
 	while (1){
-		welcome();
-		while (1){
-			printf("Au tour du joueur\n\n");
-			action();
-			action();
-			printf("Au tour de l'ordi\n\n");
-			iaction();
-			iaction();
+		int choix;
+		choix 		= welcome(&joueur);
+		joueur.vie 	= 20;
+		ordi.vie 	= 20;
+		if (choix == 0){
+			joueur.arme = ak_47;
 		}
+
+		if (choix == 1){
+			joueur.arme = m4a1_s;
+		}
+
+		while (1){
+
+			printf("Au tour du joueur\n\n");
+			action(&ordi, &joueur);
+			if(!end(&ordi, &joueur))
+				break;
+			action(&ordi, &joueur);
+			if(!end(&ordi, &joueur))
+				break;
+
+			printf("Au tour de l'ordi\n\n");
+			iaction(&ordi, &joueur);
+			if(!end(&ordi, &joueur))
+				break;
+			iaction(&ordi, &joueur);
+			if(!end(&ordi, &joueur))
+				break;
+		}
+
 	}
-	return 0;
+
+	return (0);
 }
